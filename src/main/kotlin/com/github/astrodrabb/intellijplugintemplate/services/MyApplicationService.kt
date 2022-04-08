068913f23dd0aba8ef777fabce44ae700f7b2ac0
GitHub Action <action@github.com>
2022-04-08 14:43:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/astrodrabb/intellijplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.astrodrabb.intellijplugintemplate.services
+
+import com.github.astrodrabb.intellijplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
